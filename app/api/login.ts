56017a8cbff151199d6c6b85730bad5eaedc8083
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-11-03 13:03:32 -0400

    format

--- app/api/login.ts
+++ app/api/login.ts
@@ -7,33 +7,33 @@ const prisma = new PrismaClient();
 const secret = env.JWT_SECRET || "";
 
 export async function POST(request: Request) {
-	const params = (await request.json()) as {
-		username: string;
-		password: string;
-	};
+  const params = (await request.json()) as {
+    username: string;
+    password: string;
+  };
 
-	const user = await prisma.user.findFirst({
-		where: {
-			username: {
-				equals: params.username,
-			},
-		},
-	});
+  const user = await prisma.user.findFirst({
+    where: {
+      username: {
+        equals: params.username,
+      },
+    },
+  });
 
-	if (user?.password === params.password) {
-		const token = jwt.sign(
-			{
-				id: user.id,
-			},
-			secret,
-			{ expiresIn: "1h" },
-		);
+  if (user?.password === params.password) {
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      secret,
+      { expiresIn: "1h" },
+    );
 
-		return NextResponse.json({ token }, { status: 201 });
-	} else {
-		return NextResponse.json(
-			{ err: "Invalid Username or Password" },
-			{ status: 400 },
-		);
-	}
+    return NextResponse.json({ token }, { status: 201 });
+  } else {
+    return NextResponse.json(
+      { err: "Invalid Username or Password" },
+      { status: 400 },
+    );
+  }
 }
